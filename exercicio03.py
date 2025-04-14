61c81cc095813a3e2f5f56df955673dd37c80b3b
AlanaMaria <mariaalana730@gmail.com>
2025-04-14 15:39:47 -0300

    exercicios aula06

---
+++ exercicio03.py
@@ -0,0 +1,11 @@
+n1 = int(input("Digite o numero 1:"))
+n2 = int(input("Digite o numero 2:"))
+
+divisao = n1/n2
+
+while n2==0:
+  n2 = int(input("Digite novamente o numero 2:"))
+
+divisao= n1/n2
+
+print(f"O valor será de : {divisao}")
