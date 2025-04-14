caacaca2828a6d56bf9c0e6390835327e8818666
AlanaMaria <mariaalana730@gmail.com>
2025-04-14 17:29:16 -0300

    repetir calculo

--- exercicio05.py
+++ exercicio05.py
@@ -1,9 +1,12 @@
-nota1= int(input("Digite a primeria nota:"))
-while nota1 >10 or nota1 <0:
-    nota1 = int(input("Digite novamente a primeira nota:"))
+resp= "sim"
+while resp=="sim":
+    nota1= int(input("Digite a primeria nota:"))
+    while nota1 >10 or nota1 <0:
+        nota1 = int(input("Digite novamente a primeira nota:"))
 
-nota2= int(input("Digite a segunda nota:"))
-while nota2 >10 or nota2 <0:
-    nota2 = int(input("Digite novamente a segunda nota:"))
-Divisao= nota1/nota2
-print(f"A media sera {Divisao}")
+    nota2= int(input("Digite a segunda nota:"))
+    while nota2 >10 or nota2 <0:
+        nota2 = int(input("Digite novamente a segunda nota:"))
+    media= (nota1+nota2)/2
+    print(f"A media sera {media}")
+    resposta= input("Deseja repetir o calculo?")
